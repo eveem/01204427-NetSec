c17017438375de104fe6451303a2738c20c8020d
eveem <monpriya.tammavong@gmail.com>
2018-08-30 01:46:34 +0700

    edit server distribution

--- week3/convert_unixtime_to_time.py
+++ week3/convert_unixtime_to_time.py
@@ -7,18 +7,21 @@ hour_data = {}
 for line in fn:
     ts = int(line)
     ts /= 1000.0
-    x = datetime.fromtimestamp(ts).strftime('%H')
-    x += '.00'
+    x = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print(x)
+    # x += '.00'
     if x in hour_data:
         hour_data[x] += 1
     else:
         hour_data[x] = 1
 
+# print(hour_data)
+
 hour_data_l = sorted(hour_data)
-print(hour_data_l)
+# print(hour_data_l)
 
 x = []
 for i in hour_data_l:
     x.append(hour_data[i])
 
-print(x)
+# print(x)
